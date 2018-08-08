0a5d8621c1a6ee424cf8531737309fd255c180d9
Casie Siekman <casie.siekman@gmail.com>
2018-08-08 11:03:39 -0500

    Lab 2 completed successfully

---
+++ lab-02-first-component/src/hello/Hello.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            greeting: 'Howdy!!'
+        };
+    }
+    
+    render() {
+        return (
+            <div className="hello">
+                <h1>{this.state.greeting}</h1>
+                <h2>{this.props.friend}</h2>
+                <p>Congrats! First React Component!</p>
+            </div>
+        );
+    }
+}
+
+Hello.defaultProps = {
+    friend: 'Partner!',
+};
+
+export default Hello;
